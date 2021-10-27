cdf90860460eb72eac3faecc49e5d68e0dce56c2
jawnchuks <jawnchuks@gmail.com>
2021-10-27 16:43:50 +0100

    Added new pages

---
+++ src/components/Theme/Theme.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: "#006400"
+      },
+      secondary: {
+        main: "#ffa500"
+      },
+      accent: {
+        main: "#ffa500"
+      }
+    }
+  });
